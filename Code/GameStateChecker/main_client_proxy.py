789419aca37751986421e6c889ad73809a89f9cd
AGAPIA <pciprian_ionut@yahoo.com>
2021-08-31 19:42:56 +0300

    Annotations support for CV deep learning model

--- Code/GameStateChecker/main_client_proxy.py
+++ Code/GameStateChecker/main_client_proxy.py
@@ -3,6 +3,7 @@ import jsonpickle
 import numpy as np
 import cv2
 import LogicLayer
+import json
 
 import main_flask_client as adapterClient
 
@@ -16,11 +17,37 @@ def check_visuals_proxy():
     data = request.data
     formData = request.form
     frameCount = int(formData["frameCount"])
-    imgUploaded = request.files["fileUpload"].read()
+    imgBytes = request.files["fileUpload"].read()
+    annotations_jsonStr = formData["annotations"]
+
+    annotations_deserialized = json.loads(annotations_jsonStr)
 
     # request.form to get form parameter
-    outImg = open(f"screenShot_{frameCount}.png", "wb")
-    outImg.write(imgUploaded)
+    filePath = f"screenShot_{frameCount}.png"
+    outImg = open(filePath, "wb")
+    outImg.write(imgBytes)
+
+
+    img_numpy = np.fromstring(imgBytes, dtype="uint8")
+    img_decoded = cv2.imdecode(img_numpy, cv2.IMREAD_UNCHANGED)
+    imgRes = img_decoded.shape
+
+
+    for entityName, entityRectDict in annotations_deserialized.items():
+        bboxWidth = int(entityRectDict['width'])
+        bboxHeight = int(entityRectDict['height'])
+        bboxCenter = (int(entityRectDict['x']), imgRes[0] - int(entityRectDict['y']))
+        #bboxMin = (int(bboxCenter[0] - bboxWidth/2), int(bboxCenter[1] - bboxHeight/2))
+        #bboxMax = (int(bboxCenter[0] + bboxWidth/2), int(bboxCenter[1] + bboxHeight/2))
+        bboxMin = (int(bboxCenter[0]), int(bboxCenter[1] - bboxHeight))
+        bboxMax = (int(bboxCenter[0] + bboxWidth), int(bboxCenter[1]))
+
+        color = (0, 0, 255)
+        thickness = 5
+        img_decoded = cv2.rectangle(img_decoded, bboxMin, bboxMax, color, thickness)
+
+    img_decoded = np.array(img_decoded)
+    outAugImg = cv2.imwrite("aug_"+filePath, img_decoded)
 
     #adapterClient.GameStateChecker_Client.check_visuals_onScreenshot
     return jsonify(200)
