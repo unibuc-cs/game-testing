c8ecd544fa251e9d38e8ca604a93dfdcb0528b68
AGAPIA <pciprian_ionut@yahoo.com>
2022-11-01 19:46:28 +0200

    Updated TestsPriorityApp

--- Code/TestsPriorityApp/BlueprintTesterConfig.Designer.cs
+++ Code/TestsPriorityApp/BlueprintTesterConfig.Designer.cs
@@ -31,20 +31,26 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.Run = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.BlueprintName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.MinutesSinceModified = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Category = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textPathToProject = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.BlueprintName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MinutesSinceModified = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Category = new System.Windows.Forms.DataGridViewComboBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Run,
@@ -55,28 +61,106 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(690, 869);
+            this.dataGridView1.Size = new System.Drawing.Size(1273, 869);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(749, 61);
+            this.button1.Location = new System.Drawing.Point(1340, 287);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(363, 54);
+            this.button1.Size = new System.Drawing.Size(382, 54);
             this.button1.TabIndex = 1;
             this.button1.Text = "Run Tests";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(753, 404);
+            this.button2.Location = new System.Drawing.Point(1340, 374);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(388, 90);
+            this.button2.Size = new System.Drawing.Size(388, 63);
             this.button2.TabIndex = 2;
             this.button2.Text = "Configure clusters";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1480, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "60";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1336, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Time to run";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1639, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "min";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1341, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Current Status";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(1476, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "IDLE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1336, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Path to analyze";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textPathToProject
+            // 
+            this.textPathToProject.Location = new System.Drawing.Point(1480, 51);
+            this.textPathToProject.Name = "textPathToProject";
+            this.textPathToProject.Size = new System.Drawing.Size(146, 26);
+            this.textPathToProject.TabIndex = 8;
+            this.textPathToProject.Text = "C:\\Users\\paduraruc\\Documents\\Unreal Projects\\CitySample";
+            this.textPathToProject.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1643, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Run
             // 
@@ -110,7 +194,8 @@
             "Physics",
             "Rendering",
             "Sound",
-            "Systems"});
+            "Systems",
+            "AI"});
             this.Category.MinimumWidth = 8;
             this.Category.Name = "Category";
             this.Category.ReadOnly = true;
@@ -118,58 +203,14 @@
             this.Category.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             this.Category.Width = 150;
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(859, 153);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(122, 26);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(749, 153);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Time to run";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1005, 156);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "min";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(749, 317);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 20);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Current Status";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.label4.Location = new System.Drawing.Point(889, 316);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 20);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "IDLE";
-            // 
             // BlueprintTesterConfig
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1155, 945);
+            this.ClientSize = new System.Drawing.Size(1889, 945);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textPathToProject);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -180,6 +221,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "BlueprintTesterConfig";
             this.Text = "BlueprintTesterConfig";
+            this.Load += new System.EventHandler(this.BlueprintTesterConfig_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -191,14 +233,17 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridViewCheckBoxColumn Run;
-        private System.Windows.Forms.DataGridViewTextBoxColumn BlueprintName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn MinutesSinceModified;
-        private System.Windows.Forms.DataGridViewComboBoxColumn Category;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textPathToProject;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Run;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BlueprintName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MinutesSinceModified;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Category;
     }
 }
--- Code/TestsPriorityApp/BlueprintTesterConfig.cs
+++ Code/TestsPriorityApp/BlueprintTesterConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,15 +16,17 @@ namespace TestsPriorityApp
         public BlueprintTesterConfig()
         {
             InitializeComponent();
+        }
 
+        void AddDataGridRow(bool selectedForRun, string name, int minutesSinceModified, string category)
+        {
             DataGridViewRow row = new DataGridViewRow();
-            row.CreateCells(dataGridView1);  // this line was missing
-            row.Cells[0].Value = true;
-            row.Cells[1].Value = "BP_Pathfinding_1";
-            row.Cells[2].Value = 12;
-            row.Cells[3].Value = 1;
+            row.CreateCells(dataGridView1);
+            row.Cells[0].Value = selectedForRun;
+            row.Cells[1].Value = name;
+            row.Cells[2].Value = minutesSinceModified;
+            row.Cells[3].Value = category;
             dataGridView1.Rows.Add(row);
-
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -47,6 +50,70 @@ namespace TestsPriorityApp
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textPathToProject.Text.Length  ==  0 || !System.IO.Directory.Exists(textPathToProject.Text))
+            {
+                string messageBoxText = "The given path is invalid";
+                string caption = "Error";
+                System.Windows.Forms.MessageBoxButtons buttons = MessageBoxButtons.OK;
+
+                DialogResult result = MessageBox.Show(messageBoxText, caption, buttons, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            string[] allFilesPath = Directory.GetFiles(textPathToProject.Text, "BP_*.uasset", SearchOption.AllDirectories);
+
+            foreach (string file in allFilesPath)
+            {
+                DateTime modified = File.GetLastWriteTime(file);
+                int minutesModifiedSince = (DateTime.Now - modified).Minutes;
+
+                
+                string fullPath = Path.GetFullPath(file).TrimEnd(Path.DirectorySeparatorChar);
+                
+                string resPath = file;
+                int indexOfContent = fullPath.IndexOf("Content");
+                if (indexOfContent != -1)
+                {
+                    resPath = fullPath.Substring(indexOfContent + "Content".Length + 1);
+                }
+
+
+                string category = "Gameplay";
+                if (resPath.Contains("AI"))
+                {
+                    category = "AI";
+                }
+                else if (resPath.Contains("Crowd") || resPath.Contains("Characters"))
+                {
+                    category = "Animation";
+                }
+                else if (resPath.Contains("UI"))
+                {
+                    category = "UI";
+                }
+
+                AddDataGridRow(true, resPath, minutesModifiedSince, category);
+            }
+        }
+
+        private void BlueprintTesterConfig_Load(object sender, EventArgs e)
         {
 
         }
